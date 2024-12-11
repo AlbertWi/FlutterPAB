1bbfb48baa7b63839a1e339694d5732bc79083a3
Albert Wijaya <albertwijaya_2226240029@mhs.mdp.ac.id>
2024-12-11 23:45:11 +0700

    search screen + top search screen
    dahla

--- flutter_login/lib/services/api_services.dart
+++ flutter_login/lib/services/api_services.dart
@@ -3,6 +3,8 @@ import 'dart:developer';
 
 import 'package:flutter_login/common/utils.dart';
 import 'package:flutter_login/models/movie_model.dart';
+import 'package:flutter_login/models/movie_recomendation.dart';
+import 'package:flutter_login/models/search_model.dart';
 import 'package:flutter_login/models/tv_series_model.dart';
 import 'package:http/http.dart' as http;
 
@@ -45,5 +47,32 @@ class ApiServices {
       return TvSeriesModel.fromJson(jsonDecode(response.body));
     }
     throw Exception('failed to load top rated tvseries');
+  }
+
+  Future<SearchModel> getSearchedMovie(String searchText) async {
+    endPoint = 'search/movie?query=$searchText';
+    final url = '$baseUrl$endPoint';
+    (url);
+    final response = await http.get(Uri.parse(url), headers: {
+      'Authorization':
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJmNjg2YTYxYjdkZjQ1NmJlYTYxMWU2MTg3ZDZhOTg0NiIsIm5iZiI6MTczMzc2NTY5OS4wOTYsInN1YiI6IjY3NTcyYTQzOTk3ZDk1NjQyMmY0NjczOCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.YBKcjHezBDsAgZdOl4KoEN5cIz9lXu9s4PiX3icarbc"
+    });
+    if (response.statusCode == 200) {
+      log("Succes response");
+      return SearchModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception('failed to load searched Movies');
+  }
+
+  Future<MovieRecommendationsModel> getPopularMovies() async {
+    endPoint = 'movie/popular';
+    final url = '$baseUrl$endPoint$key';
+
+    final response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      log("Succes response ");
+      return MovieRecommendationsModel.fromJson(jsonDecode(response.body));
+    }
+    throw Exception('failed to load popilar Movies');
   }
 }
